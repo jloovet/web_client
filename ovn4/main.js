3aa00b6ab2d100e66898abf3f00d7d4637f66a67
Johan Lööv <johan.loov@folksam.se>
2022-11-09 16:55:02 +0100

    show movie details

--- ovn4/main.js
+++ ovn4/main.js
@@ -27,7 +27,7 @@ form.addEventListener("submit", function (event) {
 	if (this.elements.query.value == "") {
 		alert("You must add a search string!");
 		return;
-	} 
+	}
 
 	console.log(this.elements.query.value);
 
@@ -39,8 +39,6 @@ form.addEventListener("submit", function (event) {
 	//var omdbURL = "https://www.omdbapi.com/?s=batman&type=movie&apikey=41059430";
 	// s = flera svar och då funkar ej loopen
 	var omdbURL = buildURL(this.elements.query.value, false);
-
-
 	// Vad vill vi göra när vi fått ett svar?
 	omdbAPI.addEventListener("load", function () {
 		// Konvertera resultatet från JSON
@@ -78,7 +76,7 @@ form.addEventListener("submit", function (event) {
 	omdbAPI.open("get", omdbURL, true);
 	// Skicka förfrågan
 	omdbAPI.send();
-		
+
 	addText("En film");
 
 
@@ -88,14 +86,14 @@ form.addEventListener("submit", function (event) {
 });
 
 function buildURL(searchString, exactTitle) {
-	if (exactTitle) 
+	if (exactTitle)
 		return "https://www.omdbapi.com/?t=" + searchString + "&type=movie&apikey=41059430";
 	else
 		return "https://www.omdbapi.com/?s=" + searchString + "&type=movie&apikey=41059430";
 }
 
 function parseJSON(result, exactTitle) {
-	clearTable() 
+	clearMovieTable()
 	if (exactTitle) {
 		for (var key in result) {
 			if (result.hasOwnProperty(key)) {
@@ -125,54 +123,80 @@ function addText(text) {
 }
 
 function addTableRow(rownumb, title, year, imdbID, poster) {
-	var table = document.getElementById("myTable");
+	var table = document.getElementById("movieTable");
 	var row = table.insertRow(1);
 	var cell1 = row.insertCell(0);
 	var cell2 = row.insertCell(1);
 	var cell3 = row.insertCell(2);
-	var cell4 = row.insertCell(3);	
-	var cell5 = row.insertCell(4);	
+	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
 	cell1.innerHTML = title;
 	cell2.innerHTML = year;
 	cell3.innerHTML = imdbID;
 
-	//var link = '<a href="localhost:8080/getDetails/' +'"></a>';
-	var link  = '<a href="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">Go to Movie Poster</a>';
+	var link = '<a href="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">Go to Movie Poster</a>';
 	cell4.innerHTML = link;
 
-	//let button = document.createElement('button');
-	//cell5.innerHTML = button;
-
-	/*
-	let button = document.createElement('button');
-	button.id = "but_" + rownumb;
-	button.class = "rowbutton";
-	button.addEventListener('onclick', aTest);
-	cell5.appendChild(button);
-	*/
-
-
-	//cell5.innerHTML = '<button id = but_' +rownumb+ ' class="rowbutton" onclick="aTest(this);"> Show more information </button>';
-	cell5.innerHTML = '<button id = "' +title+ '" class="rowbutton" onclick="aTest(this);"> Show more information </button>';
-
-
-	//lägg till en "show grade"-knapp som sista kolumn och visa i popup (eller i samma kolumn som knapp)
+	cell5.innerHTML = '<button id = "' + title + '" class="rowbutton" onclick="moreInfo(this);"> Show more information </button>';
 }
 
 
-function aTest(but) {
-	alert("YYu pressed the button " + but.id);
+function addDetailsTableRow(col1, col2) {
+	var table = document.getElementById("detailsTable");
+	var row = table.insertRow(1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	cell1.innerHTML = col1;
+	cell2.innerHTML = col2;
 }
 
-function clearTable() {
-	var table = document.getElementById("myTable");
-	var rowCount = table.rows.length;
-	for (var i = rowCount - 1; i > 0; i--) {
-		table.deleteRow(i);
+
+function moreInfo(but) {
+	//alert("YYu pressed the button " + but.id);
+	document.getElementById("headId").innerHTML = but.id;
+
+	var omdbAPI2 = new XMLHttpRequest();
+	var omdbURL2 = buildURL(but.id, true);
+	omdbAPI2.addEventListener("load", handleDetails);
+	omdbAPI2.open("get", omdbURL2, true);
+	omdbAPI2.send();
+}
+
+function handleDetails() {
+	//alert("got details " + this.responseText);
+	var result = JSON.parse(this.responseText);
+	//parseJSON(result, true);
+	addDetailsTableRow("Released", result.Released)
+	addDetailsTableRow("Runtime", result.Runtime)
+	addDetailsTableRow("Director", result.Director)
+	addDetailsTableRow("Actors", result.Actors)
+}
+
+
+
+function clearMovieTable() {
+	var table = document.getElementById("movieTable");
+	try {
+		var rowCount = table.rows.length;
+		for (var i = rowCount - 1; i > 0; i--) {
+			table.deleteRow(i);
+		}
+	} catch (error) { 
+		//if table got no rows obj yet, we end up here}
 	}
 }
 
+function clearDetailsTable() {
+	var table = document.getElementById("detailsTable");
+	try {
+		var rowCount = table.rows.length;
+		for (var i = rowCount - 1; i > 0; i--) {
+			table.deleteRow(i);
+		}
+	} catch (error) { 
+		//if table got no rows obj yet, we end up here}
+	}
 
-
+}
 
 
