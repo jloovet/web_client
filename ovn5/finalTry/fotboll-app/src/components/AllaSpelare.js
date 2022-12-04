beec48ea6552fe3da297d0bee2bcecc2438b73d4
jloovet <jloovet@gmail.com>
2022-12-04 21:00:35 +0100

    minor

--- ovn5/finalTry/fotboll-app/src/components/AllaSpelare.js
+++ ovn5/finalTry/fotboll-app/src/components/AllaSpelare.js
@@ -29,7 +29,7 @@ class AllaSpelare extends React.Component {
       {
         id: uuidv4(),
         spelare: "Sven-hugo Ericsson",
-        position: "Målvalt",
+        position: "Målvakt",
         gradering: "Enastående!"
 
       },
